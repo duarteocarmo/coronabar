a0a7c207ac565702d1c34cf7e47f0042de362eba
Duarte OC <duarteocarmo@gmail.com>
2020-07-28 10:19:16 +0100

    black formattting

--- setup.py
+++ setup.py
@@ -9,37 +9,39 @@ INSTALL = []
 SCRIPT = []
 PACKAGES = []
 
-if sys.platform.startswith('darwin'):
-    APP = ['app.py']
+if sys.platform.startswith("darwin"):
+    APP = ["app.py"]
     DATA_FILES = []
-    OPTIONS = {'py2app': {
-        'argv_emulation': True,
-        'iconfile': 'icon.icns',
-        'plist': {
-            'CFBundleShortVersionString': '0.2.0',
-            'LSUIElement': True,
-        },
-        'packages': ['rumps', 'requests'],
-    }}
-    SETUP = ['py2app']
-    INSTALL =  ['rumps', 'requests']
+    OPTIONS = {
+        "py2app": {
+            "argv_emulation": True,
+            "iconfile": "icon.icns",
+            "plist": {
+                "CFBundleShortVersionString": "0.2.0",
+                "LSUIElement": True,
+            },
+            "packages": ["rumps", "requests"],
+        }
+    }
+    SETUP = ["py2app"]
+    INSTALL = ["rumps", "requests"]
     SCRIPT = []
-elif sys.platform.startswith('linux'):
-    APP = ['app.py']
+elif sys.platform.startswith("linux"):
+    APP = ["app.py"]
     DATA_FILES = []
     SETUP = []
-    SCRIPT = ['bin/coronabar']
+    SCRIPT = ["bin/coronabar"]
     PACKAGES = find_packages()
-    
+
 
 setup(
-        app=APP,
-        name='CoronaBar',
-        packages=PACKAGES,        
-        data_files=DATA_FILES,
-        options=OPTIONS,
-        setup_requires=SETUP,
-        install_requires=INSTALL,
-        include_package_data=True,
-        scripts=SCRIPT,
-        )
+    app=APP,
+    name="CoronaBar",
+    packages=PACKAGES,
+    data_files=DATA_FILES,
+    options=OPTIONS,
+    setup_requires=SETUP,
+    install_requires=INSTALL,
+    include_package_data=True,
+    scripts=SCRIPT,
+)
